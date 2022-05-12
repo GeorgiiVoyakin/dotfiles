acc5f8f4013f4d985e55dfb235a01cddb2e462c0
Georgii Voyakin <voyaking@gmail.com>
2022-05-12 15:19:56 +0300

    Add LSP, Telescope and complete

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,25 @@
 require('plugins')
 
+local opts = { noremap = true, silent = true }
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 -- Set colorscheme
-vim.g.tokyonight_style = "night"
+vim.g.tokyonight_style = 'night'
 vim.cmd[[colorscheme tokyonight]]
 
 vim.opt.number = true -- show line numbers
 vim.opt.termguicolors = true -- 24-bit RGB colors
+vim.g.mapleader = ' ' -- leader key
 -- Tabs
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- Turn off arrow keys
-vim.api.nvim_set_keymap('', '<Up>', '<Nop>', {noremap = true})
-vim.api.nvim_set_keymap('', '<Down>', '<Nop>', {noremap = true})
-vim.api.nvim_set_keymap('', '<Left>', '<Nop>', {noremap = true})
-vim.api.nvim_set_keymap('', '<Right>', '<Nop>', {noremap = true})
+vim.keymap.set({'', 'i'}, '<Up>', '<Nop>', opts)
+vim.keymap.set({'', 'i'}, '<Down>', '<Nop>', opts)
+vim.keymap.set({'', 'i'}, '<Left>', '<Nop>', opts)
+vim.keymap.set({'', 'i'}, '<Right>', '<Nop>', opts)
 
 -- Lualine
 require('lualine').setup {
@@ -23,3 +27,54 @@ require('lualine').setup {
     theme = 'tokyonight'
   }
 }
+
+-- LSP
+local lsp_opts = { buffer = 0, noremap = true, silent = true }
+
+local on_attach = function()
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, lsp_opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, lsp_opts)
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, lsp_opts)
+    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, lsp_opts)
+end
+
+local servers = { 'gopls' }
+for _, lsp in pairs(servers) do
+    require('lspconfig')[lsp].setup{
+        capabilities = capabilities,
+        on_attach = on_attach,
+    }
+end
+
+-- Telescope
+require('telescope').load_extension('fzf')
+
+-- Setup nvim-cmp.
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+local cmp = require'cmp'
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    }, {
+        { name = 'buffer' },
+        { name = 'path' },
+    })
+})
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,9 +3,26 @@ return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
     -- Tokyonight colorscheme
     use 'folke/tokyonight.nvim'
+    -- Lualine
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
-    use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+    -- Collection of configurations for the built-in LSP client
+    use 'neovim/nvim-lspconfig'
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' } -- Telescope extension for better and faster sorting
+    -- Devicons
+    use 'kyazdani42/nvim-web-devicons'
+    -- Autocomplete
+    use 'hrsh7th/nvim-cmp' -- Completion engine
+    use 'hrsh7th/cmp-nvim-lsp' -- Source for neovim's built-in language server client
+    use 'hrsh7th/cmp-buffer' -- Source for current buffer
+    use 'hrsh7th/cmp-path' -- Source for filesystem paths
+    use 'L3MON4D3/LuaSnip' -- Snippet engine
+    use 'saadparwaiz1/cmp_luasnip' -- Luasnip completion source for nvim-cmp
 end)
